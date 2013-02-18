86a2742af3d3b92c6ed065f00232910c4edc2071
Ivana Setiawan <ivana@mangrove.nl>
2013-02-18 17:09:39 +0100

    Add awesomeshizzle

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-
+	paralex();
 });
+
+function paralex() {
+	$(window).scroll(function(){
+		var body = $('body').scrollTop();
+		$('.mainheader img').css('-webkit-transform', 'translateY(' + (body/5) + 'px)');
+	});
+}
