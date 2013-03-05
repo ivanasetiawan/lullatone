0795d137978152353e889b7e8b398a6632310257
Ivana Setiawan <ivana@mangrove.nl>
2013-03-05 16:53:53 +0100

    Trying parallex

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
-	paralex();
+	parallex();
+	trialError();
 });
 
-function paralex() {
+function parallex() {
 	$(window).scroll(function(){
 		var body = $('body').scrollTop();
 		$('.mainheader img').css('-webkit-transform', 'translateY(' + (body/2) + 'px)');
+	});
+}
+
+function trialError() {
+	$('a').bind({
+		'click' : function(e) {
+			console.log('clicked');
+		},
+		'mouseenter' : function(f) {
+			console.log('mouseenter');
+		},
+		'mouseleave' : function(g) {
+			console.log('mouseleave');
+		}
 	});
 }
